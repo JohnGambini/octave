6d73bef20cd6983602d7cbbc3c02c3c8f5248fb6
john gambini <john.a.gambini@gmail.com>
2019-01-30 20:09:38 -0500

    Added boiler plate initialization

--- basic-plots/point.m
+++ basic-plots/point.m
@@ -1,4 +1,10 @@
-plot3(1,2,3,"r");
+# point.m
+#
+# Copyright 2015 2016 2017 2018 by John Gambini
+#
+clear all; clc; close all;
+
+plot3(1,2,3,'.','markersize',12,'color', 'b');
 
 xlabel x;
 ylabel y;
--- basic-plots/simple-line.m
+++ basic-plots/simple-line.m
@@ -2,5 +2,5 @@
 #
 # Copyright 2015 2016 2017 2018 by John Gambini
 #
-clf;
+clear all; clc; close all;
 plot3([0 1],[0 1],[0 1]);
